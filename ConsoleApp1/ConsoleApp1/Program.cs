501ed534648a7ad1644ee2fe4bf56d39bc741642
KarmaG2020 <59843592+KarmaG2020@users.noreply.github.com>
2022-09-14 11:42:49 -0500

    Console App

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using ConsoleApp1;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using System;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            CreatHostBuilder(args).Build().Run();
+        }
+        public static IHostBuilder CreatHostBuilder(String[] args) =>
+        
+            Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            }); 
+        
+    }
+
+}
